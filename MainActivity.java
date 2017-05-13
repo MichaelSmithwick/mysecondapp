d1fa4a6cdd0d2d26abcd7bb34d9c895090f7fe24
MichaelSmithwick <MichaelSmithwick@users.noreply.github.com>
2017-05-12 20:04:38 -0700

    It's working
    Cleaned up the code and everything is working again.
    The record and playback functionality is now successfully broken out
    into functions where they can be called independently. The
    recommendation is the caller should call them in a thread in order not
    to block the main application.

--- MainActivity.java
+++ MainActivity.java
@@ -1,18 +1,13 @@
 package com.meinc.mysecondapp;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.media.AudioAttributes;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.os.Environment;
-import android.os.storage.StorageManager;
-import android.os.storage.StorageVolume;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,8 +21,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Static members of the MainActivity class may be prefixed with 'M2A' to distinguish them
@@ -37,16 +30,13 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity{
    private static final String LOG_TAG="com.meinc.mysecondapp";
    private static final int M2A_FREQ=44100;
-   private boolean playBackGuard=false;
+   private static final String APP_STORAGE="dogWhistle";
+   private static final String DEFAULT_FN="record1.m2a";
 
-   private int counter;
-   private Thread Rthread;
-
-   // AudioManager permits volume changes, ring changes, and other interface info and changes
-   //private AudioManager audioManager;
-   private AudioTrack audioTrack;
-   private boolean threadStop;
-   private File storagePath;
+   private boolean playBackGuard=false; /** permits only single access to playback mechanism */
+   private int counter=0; /** Counts how many times the 'Check Permissions' button has been pushed */
+   private boolean threadStop=false; /** Stops the sound input loop */
+   private File storagePath=null; /** Holds the storage path for the application */
 
    /**
     * Sets up initial UI
@@ -73,16 +63,13 @@ public class MainActivity extends AppCompatActivity{
     */
    public MainActivity(){
       counter=0;
-      Rthread=null;
-      //audioManager=null;
-      audioTrack=null;
       threadStop=false;
-      storagePath=getAudioStorageDir("dogWhistle");
+      storagePath=getAudioStorageDir(APP_STORAGE);
    }
 
    /**
-    * Pushing the Check Permissions button calls this function
-    * @param view
+    * Pushing the 'Check Permissions' button calls this function
+    * @param view The view that called the function? i.e. the Button??
     */
    public void checkNow(View view){
       showInfo((TextView)findViewById(R.id.informationBox));
@@ -96,7 +83,7 @@ public class MainActivity extends AppCompatActivity{
     *    The path to the app data
     * @param textView The TextView to send the data to
     */
-   public void showInfo(TextView textView){
+   private void showInfo(TextView textView){
       counter++;
       String infoString=counter+"\n"+this.getText(R.string.hello_world)+"\n";
       textView.setText(infoString);
@@ -119,10 +106,7 @@ public class MainActivity extends AppCompatActivity{
     */
    private boolean isExternalStorageWriteable(){
       String state=Environment.getExternalStorageState();
-      if(Environment.MEDIA_MOUNTED.equals(state)){
-         return true;
-      }
-      return false;
+      return Environment.MEDIA_MOUNTED.equals(state);
    }
 
    /**
@@ -142,6 +126,17 @@ public class MainActivity extends AppCompatActivity{
    }
 
    /**
+    * Add the string to the UI. The string is appended to the current displayed data.
+    * @param addString string to add to the current UI
+    */
+   private void updateMessageBoard(String addString){
+      EditText textView=(EditText)findViewById(R.id.informationBox);
+      String current=textView.getText().toString();
+      current+=addString;
+      textView.setText(current);
+   }
+
+   /**
     * This function is called when the Record Sound button is pushed
     * threadStop must be set to false to permit the recording of sound until
     * the 'Stop Recording' button is pushed. The Stop Recording button handler
@@ -155,37 +150,92 @@ public class MainActivity extends AppCompatActivity{
    }
 
    /**
-    * Add the string to the UI. The string is appended to the current displayed data.
-    * @param addString string to add to the current UI
-    */
-   public void updateMessageBoard(String addString){
-      EditText textView=(EditText)findViewById(R.id.informationBox);
-      String current=textView.getText().toString();
-      current+=addString;
-      textView.setText(current);
-   }
-
-   /**
     * This function sets up the microphone input and speaker output,
     * and runs the recording of sound in a thread.
     * The recorded sound is stored in a file. After the user ends the
     * recording the file is read back and played through the speakers.
     */
-   protected void loopback(){
+   private void loopback(){
       android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
-      // The thread that will capture, store, and replay the sound is created here
-      // The Runnable object's run() function is created here but will be called
-      // when the thread is started later on.
-      Rthread=new Thread(new Runnable(){
+      Thread Rthread=new Thread(new Runnable(){
          @Override
          public void run(){
-            record();
-            while(!threadStop);
-            playBack();
+            xRecord(DEFAULT_FN);
+            xPlayBack(DEFAULT_FN,3*1024);
          }
       });
-      // The thread is started here. It calls the run() function defined above.
       Rthread.start();
+   }
+
+   /**
+    * Opens the microphone and records input sound.
+    * Saves it in the application data directory using the filename provided
+    * @param filename The filename for the file storing the audio input data
+    */
+   private void xRecord(String filename){
+      final int bufferSize=AudioRecord.getMinBufferSize(M2A_FREQ,AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT);
+      final AudioRecord audioRecord=new AudioRecord(MediaRecorder.AudioSource.MIC,M2A_FREQ,AudioFormat.CHANNEL_IN_MONO,MediaRecorder.AudioEncoder.AMR_NB,bufferSize);
+      audioRecord.startRecording();
+      final File outFile = new File(storagePath,filename);
+      try{
+         final byte[] buffer=new byte[bufferSize];
+         FileOutputStream outFileStream=new FileOutputStream(outFile);
+         while(!threadStop){
+            try{
+               audioRecord.read(buffer,0,bufferSize);
+               outFileStream.write(buffer,0,bufferSize);
+            }catch(Throwable T){
+               Log.e(LOG_TAG,"Read/Write Failed");
+               T.printStackTrace();
+            }
+         }
+         audioRecord.stop();
+         audioRecord.release();
+         try{
+            outFileStream.close();
+         }catch(IOException e){
+            e.printStackTrace();
+         }
+      }catch(FileNotFoundException e){
+         Log.e("com.meinc.Error","Unable to open output file");
+         e.printStackTrace();
+      }
+   }
+
+   /**
+    * Plays back  audio from the a file with the given filename stored in the application
+    * data directory. Allocates a buffer of size buffersize for transferring data between
+    * the file and audio output
+    * @param filename The name of the audio file to read
+    * @param bufferSize  The size of the transfer buffer to use (the size should be divisable by 3)
+    */
+   private void xPlayBack(String filename,int bufferSize){
+      if(playBackGuard) return;
+      playBackGuard=true;
+      AudioTrack audioTrack=new AudioTrack(AudioManager.ROUTE_SPEAKER,M2A_FREQ,AudioFormat.CHANNEL_OUT_MONO,MediaRecorder.AudioEncoder.AMR_NB,bufferSize,AudioTrack.MODE_STREAM);
+      audioTrack.setPlaybackRate(M2A_FREQ);
+      audioTrack.play();
+      final File inFile = new File(storagePath,filename);
+      try{
+         final byte[] buffer=new byte[bufferSize];
+         FileInputStream inFileStream=new FileInputStream(inFile);
+         int bytesRead=0;
+         while(bytesRead>-1){
+            try{
+               bytesRead=inFileStream.read(buffer,0,bufferSize);
+               audioTrack.write(buffer,0,bufferSize);
+            }catch(IOException e){
+               e.printStackTrace();
+            }
+         }
+         audioTrack.flush();
+         audioTrack.stop();
+         audioTrack.release();
+      }catch(FileNotFoundException e){
+         Log.e("com.meinc.Error","Unable to open input file");
+         e.printStackTrace();
+      }
+      playBackGuard=false;
    }
 
    /**
@@ -204,94 +254,13 @@ public class MainActivity extends AppCompatActivity{
     * @param view
     */
    public void playBackRecording(View view){
-      playBack();
-   }
-
-   /**
-    * Record to a file previously selected by the user
-    */
-   public void record(){
       android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
-      final int bufferSize=AudioRecord.getMinBufferSize(M2A_FREQ,AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT);
-      final AudioRecord audioRecord=new AudioRecord(MediaRecorder.AudioSource.MIC,M2A_FREQ,AudioFormat.CHANNEL_IN_MONO,MediaRecorder.AudioEncoder.AMR_NB,bufferSize);
-      final byte[] buffer=new byte[bufferSize];
-      Thread rThread=new Thread(new Runnable(){
-         // The run function opens a data file, captures sound from the microphone
-         // and saves it to the opened file.
-         // When the user selects the 'Stop Recording' button the 'threadStop'
-         // flag is set to true and the capture loop ends.
-         // The file is flushed and closed.
+      Thread pbThread=new Thread(new Runnable(){
          public void run(){
-            final File outFile = new File(storagePath,"record1.m2a");
-            try{
-               FileOutputStream outFileStream=new FileOutputStream(outFile);
-               // threadStop is set by stopRecord() function
-               while(!threadStop){
-                  try{
-                     audioRecord.read(buffer,0,bufferSize);
-                     outFileStream.write(buffer,0,bufferSize);
-                  }catch(Throwable T){
-                     Log.e(LOG_TAG,"Read/Write Failed");
-                     T.printStackTrace();
-                  }
-               }
-               audioRecord.stop();
-               audioRecord.release();
-               try{
-                  outFileStream.close();
-               }catch(IOException e){
-                  e.printStackTrace();
-               }
-               // end audio capture segment
-            }catch(FileNotFoundException e){
-               Log.e(LOG_TAG,"Unable to open output file");
-               e.printStackTrace();
-            }
+            xPlayBack(DEFAULT_FN,3*1024);
          }
       });
-
-      rThread.start();
-  }
-
-   /**
-    * Playback the file that has been previously selected by the user
-    */
-   public void playBack(){
-      if(playBackGuard) return;
-      playBackGuard=true;
-      final int bufferSize=1024;
-      android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
-      audioTrack=new AudioTrack(AudioManager.ROUTE_SPEAKER,M2A_FREQ,AudioFormat.CHANNEL_OUT_MONO,MediaRecorder.AudioEncoder.AMR_NB,bufferSize,AudioTrack.MODE_STREAM);
-      audioTrack.setPlaybackRate(M2A_FREQ);
-      final byte[] buffer=new byte[bufferSize];
-      audioTrack.play();
-
-      Thread pThread=new Thread(new Runnable(){
-         @Override
-         public void run(){
-            final File outFile = new File(storagePath,"record1.m2a");
-            try{
-               FileInputStream inFileStream=new FileInputStream(outFile);
-               int bytesRead=0;
-               while(bytesRead>-1){
-                  try{
-                     bytesRead=inFileStream.read(buffer,0,bufferSize);
-                     audioTrack.write(buffer,0,bufferSize);
-                  }catch(IOException e){
-                     e.printStackTrace();
-                  }
-               }
-               audioTrack.flush();
-               audioTrack.stop();
-               audioTrack.release();
-            }catch(FileNotFoundException e){
-               Log.e(LOG_TAG,"File Not Found");
-               e.printStackTrace();
-            }
-            playBackGuard=false;
-         }
-      });
-
-      pThread.start();
+      pbThread.start();
    }
+
 }
